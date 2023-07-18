33a064d858315de126420292b4ce1e247fa54f66
Martinas <martinas1990@mail.ru>
2023-07-18 19:20:20 +0300

    finished ui if buttons and slider

--- src/components/settings/Settings.tsx
+++ src/components/settings/Settings.tsx
@@ -1,32 +1,32 @@
 import React, {useState} from 'react';
 import s from './Settings.module.css'
 import MultiRangeSlider from "../slider/Slider";
+import Slider from "../slider/Slider";
 
 const Settings = () => {
-    const [isActive, setIsActive] = useState(false)
-    const handleChange = () => {
-
+    const [isMyActive, setIsMyActive] = useState(false)
+    const [isAllActive, setIsAllActive] = useState(false)
+    const handleAllChange = () => {
+        setIsAllActive(true)
+        setIsMyActive(false)
+    }
+    const handleMyChange = () => {
+        setIsAllActive(false)
+        setIsMyActive(true)
     }
     return (
         <div className={s.container}>
             <div>
                 <h2>Show packs cards</h2>
                 <div>
-                    <button className={s.button}>My</button>
-                    <button className={s.button}>All</button>
+                    <button onClick={handleMyChange} className={isMyActive ? s.active : s.button}>My</button>
+                    <button onClick={handleAllChange} className={isAllActive ? s.active : s.button}>All</button>
                 </div>
             </div>
             <div>
                 <h2>Number of cards</h2>
                 <div>
-                    Slider
-                    <MultiRangeSlider
-                        min={0}
-                        max={100}
-                        onChange={({ min, max }: { min: number; max: number }) =>
-                            console.log(`min = ${min}, max = ${max}`)
-                        }
-                    />
+                    <Slider/>
                 </div>
             </div>
 
--- src/components/slider/Slider.tsx
+++ src/components/slider/Slider.tsx
@@ -1,106 +1,41 @@
-import {
-    ChangeEvent,
-    FC,
-    useCallback,
-    useEffect,
-    useState,
-    useRef
-} from "react";
-// import classnames from "classnames";
-// import "./multiRangeSlider.css";
+import React, {useState} from 'react';
+import MultiRangeSlider from "multi-range-slider-react";
+import s from './Slider.module.css'
 
-interface MultiRangeSliderProps {
-    min: number;
-    max: number;
-    onChange: Function;
-}
 
-const MultiRangeSlider: FC<MultiRangeSliderProps> = ({
-                                                         min,
-                                                         max,
-                                                         onChange
-                                                     }) => {
-    const [minVal, setMinVal] = useState(min);
-    const [maxVal, setMaxVal] = useState(max);
-    const minValRef = useRef<HTMLInputElement>(null);
-    const maxValRef = useRef<HTMLInputElement>(null);
-    const range = useRef<HTMLDivElement>(null);
-
-    // Convert to percentage
-    const getPercent = useCallback(
-        (value: number) => Math.round(((value - min) / (max - min)) * 10),
-        [min, max]
-    );
-
-    // Set width of the range to decrease from the left side
-    useEffect(() => {
-        if (maxValRef.current) {
-            const minPercent = getPercent(minVal);
-            const maxPercent = getPercent(+maxValRef.current.value); // Precede with '+' to convert the value from type string to type number
-
-            if (range.current) {
-                range.current.style.left = `${minPercent}%`;
-                range.current.style.width = `${maxPercent - minPercent}%`;
-            }
-        }
-    }, [minVal, getPercent]);
-
-    // Set width of the range to decrease from the right side
-    useEffect(() => {
-        if (minValRef.current) {
-            const minPercent = getPercent(+minValRef.current.value);
-            const maxPercent = getPercent(maxVal);
-
-            if (range.current) {
-                range.current.style.width = `${maxPercent - minPercent}%`;
-            }
-        }
-    }, [maxVal, getPercent]);
-
-    // Get min and max values when their state changes
-    useEffect(() => {
-        onChange({ min: minVal, max: maxVal });
-    }, [minVal, maxVal, onChange]);
-
+const Slider = () => {
+    const [minValue, setMinValue] = useState(0);
+    const [maxValue, setMaxValue] = useState(10);
+    const handleInput = (e: any) => {
+        setMinValue(e.minValue);
+        setMaxValue(e.maxValue);
+    };
+    let numbers = ['1','2','3','4','5','6','7','8','9','10']
     return (
-        <div className="container">
-            <input
-                type="range"
-                min={min}
-                max={max}
-                value={minVal}
-                ref={minValRef}
-                onChange={(event: ChangeEvent<HTMLInputElement>) => {
-                    const value = Math.min(+event.target.value, maxVal - 1);
-                    setMinVal(value);
-                    event.target.value = value.toString();
-                }}
-                // className={classnames("thumb thumb--zindex-3", {
-                //     "thumb--zindex-5": minVal > max - 100
-                // })}
-            />
-            <input
-                type="range"
-                min={min}
-                max={max}
-                value={maxVal}
-                ref={maxValRef}
-                onChange={(event: ChangeEvent<HTMLInputElement>) => {
-                    const value = Math.max(+event.target.value, minVal + 1);
-                    setMaxVal(value);
-                    event.target.value = value.toString();
-                }}
-                className="thumb thumb--zindex-4"
-            />
-
-            <div className="slider">
-                <div className="slider__track"></div>
-                <div ref={range} className="slider__range"></div>
-                <div className="slider__left-value">{minVal}</div>
-                <div className="slider__right-value">{maxVal}</div>
+        <div>
+            <div className={s.slider}>
+                <MultiRangeSlider
+                    labels={numbers}
+                    min={0}
+                    max={10}
+                    step={1}
+                    ruler={true}
+                    label={true}
+                    preventWheel={true}
+                    minValue={minValue}
+                    maxValue={maxValue}
+                    barInnerColor={'blue'}
+                    onInput={(e) => {
+                        handleInput(e);
+                    }}
+                />
             </div>
+            {/*<div style={{ display: "inline-block", margin: "50px auto" }}>*/}
+            {/*    MinValue={minValue}*/}
+            {/*    MaxValue={maxValue}*/}
+            {/*</div>*/}
         </div>
     );
 };
 
-export default MultiRangeSlider;
+export default Slider;
